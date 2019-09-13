50c3989860c86cc7ab6d27be00bb9ee5ede286a5
Eric Blancas <ericblancas23@gmail.com>
2019-09-12 17:57:20 -0700

    added data

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MenuItem = ({title}) => (
+    <div className='menu-item'>
+        <div className='content'>
+          <div className='title'></div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
